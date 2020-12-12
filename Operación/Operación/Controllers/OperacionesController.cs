6fe05468ee993f7029fcc8c0201fd35eb76a4005
Anahi Samantha Agreda Cruz <a2019118777@estudiantes.upsa.edu.bo>
2020-12-12 11:06:25 -0400

    Se creo la funcion Operaciones

---
+++ Operación/Operación/Controllers/OperacionesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Operación.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperacionesController : ControllerBase
+    {
+        [HttpGet]
+        public string Operaciones(int numero)
+        {
+            string respuesta = "";
+            
+            if(numero < 0)
+            { respuesta = "ERROR"; }
+
+            if (numero == 0)
+            { respuesta = "Realizado por Anahi Agreda"; }
+
+            if (numero > 0)
+            { respuesta = "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg"; }
+
+            return respuesta;
+        }
+    }
+}
